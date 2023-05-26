cd9c55c3dfc8f4d15124c82666542ebc9b7ec11f
yutawatanabe0712 <127653923+yutawatanabe0712@users.noreply.github.com>
2023-05-27 00:59:59 +0900

    SpringSample　課題1
    SpringSample　課題1

--- JavaStudy/springSample/src/main/java/com/example/demo/controller/UserController.java
+++ JavaStudy/springSample/src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
@@ -26,11 +28,31 @@ public class UserController {
    * @param model Model
    * @return ユーザー情報一覧画面
    */
-  @GetMapping(value = "/user/list")
+  @RequestMapping(value = "/user/list")
   public String displayList(Model model) {
     List<UserEntity> userlist = userService.searchAll();
     model.addAttribute("userlist", userlist);
     return "user/list";
   }
 
+  /**
+* ユーザー新規登録画面を表示
+* @param model Model
+* @return ユーザー情報一覧画面
+*/
+  @GetMapping("/user/add")
+  public String displayAdd(Model model) {
+	  return "user/add";
+  }
+  
+  /**
+* ユーザー情報詳細画面を表示
+* @param id 表示するユーザーID
+* @param model Model
+* @return ユーザー情報詳細画面
+*/
+  @GetMapping("/user/{id}")
+  public String displayView(@PathVariable Integer id, Model model) {
+	  return "user/view";
+  }
 }
